793d65d28c531a5edd50a8e0aec54e6cd19b2e60
Evan <evangulyas@outlook.com>
2015-08-30 18:49:53 -0400

    added get

--- MRequest/MRequest.m
+++ MRequest/MRequest.m
@@ -8,13 +8,34 @@
 
 #import "MRequest.h"
 
+@interface MRequest ()
+
+-(void)get:(NSURL *)url completionHandler:(void(^)(NSError *, NSURLResponse *, NSData *))block;
+
+@end
+
 @implementation MRequest
 
 -(void)requestWithOptions:(NSDictionary *)options completionHandler:(void (^)(NSError *, NSURLResponse *, NSData *))block {
     
+    NSString *method = [options valueForKey:@"method"];
+    NSURL *url = [NSURL URLWithString:[options valueForKey:@"url"]];
+    
+    if ([method isEqualToString:@"get"]) {
+        
+        [self get:url completionHandler:^ (NSError *error, NSURLResponse *response, NSData *data) {
+            block(error, response, data);
+        }];
+        
+    }
+    
+}
+
+-(void)get:(NSURL *)url completionHandler:(void (^)(NSError *, NSURLResponse *, NSData *))block {
+    
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc]init];
-    [request setHTTPMethod:[options valueForKey:@"method"]];
-    [request setURL:[NSURL URLWithString:[options valueForKey:@"url"]]];
+    [request setHTTPMethod:@"get"];
+    [request setURL:url];
     
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^ (NSURLResponse *response, NSData *data, NSError *error) {
         block(error, response, data);
